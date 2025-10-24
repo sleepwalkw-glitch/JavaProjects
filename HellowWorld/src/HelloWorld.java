781c87c48f74643f62209b4a7b40801a8c3d2126
Fangyuan <sleep.walk.w@gmail.com>
2025-10-24 18:13:33 +0200

    Add HelloWorld Project

---
+++ HellowWorld/src/HelloWorld.java
@@ -0,0 +1,36 @@
+public class HelloWorld {
+    public static void main(String[] arg){
+        int number;
+        number=40;
+        double decimal = 1.29;
+        boolean vrai =true;
+        boolean faux =false;
+        char a = 'A';
+        String nom = "le Nom 2";
+        String text1= "Hello World";
+        //commentaire
+        /* commentaire
+           pour
+           plusieur
+           linges
+         */
+        //concatenation de text
+        String Newname= "Daphine";
+        int age =17;
+        boolean Etudiant=true;
+        System.out.println("Student's Name:"+Newname) ;
+        System.out.println("Age:" + age );
+        System.out.println("Identity of the student:" + Etudiant);
+        System.out.println("Student's Name: "+Newname +"     "+"Age: " + age +"    "+"Identity of the student: " + Etudiant);
+
+
+        System.out.println(number);
+        System.out.println("Hello World");
+        System.out.println(decimal);
+        System.out.println(vrai);
+        System.out.println(faux);
+        System.out.println(a);
+        System.out.println(nom);
+        System.out.println(text1);
+    }
+}
